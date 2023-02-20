b4b1704b5dcc51541171b011e88616c6a2ef93fa
Ximena Cantera <A01277310@tec.mx>
2023-02-20 11:02:34 -0600

    Laboratiorio 6

---
+++ Lab4.js
@@ -0,0 +1,21 @@
+let num = prompt("Dame un número",0);
+
+const arreglo = [1];
+
+function cuadrado(a, ){
+        let a = parseInt(num);
+        let result = a * a;
+        alert(result);
+        arreglo.push(result);
+        for (let elemento in arreglo){
+            console.log(arreglo);
+        }
+    }
+//    function cubo(a, b){
+//        var a = parseInt(numerando);
+//        var b = parseInt(numerado);
+//        var result = a - b;
+//        alert(result);
+//        alert('El resultado de ' + a + ' menos ' + b + ' es igual a ' + result);
+//    }
+

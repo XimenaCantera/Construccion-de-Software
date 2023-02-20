d7fd79a2c0db8d57c0020badd9a9a3b4bbc68688
XimenaCantera <95453390+XimenaCantera@users.noreply.github.com>
2023-02-19 18:46:44 -0600

    Create Lab4.js

---
+++ Lab4.js
@@ -0,0 +1,13 @@
+let num = prompt("Dame un número",0);
+
+const arreglo = [1];
+
+function cuadrado(a, ){
+        let a = parseInt(num);
+        let result = a * a;
+        alert(result);
+        arreglo.push(result);
+        for (let elemento in arreglo){
+            console.log(arreglo);
+        }
+    }

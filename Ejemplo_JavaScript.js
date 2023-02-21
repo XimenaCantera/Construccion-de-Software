e33eeb5696daef97af9db1ff68d29177e96b0e53
Ximena Cantera <A01277310@tec.mx>
2023-02-21 10:28:28 -0600

    lab 3 y 4

--- Ejemplo_JavaScript.js
+++ Ejemplo_JavaScript.js
@@ -1,5 +1,5 @@
 //Ximena Cantera Reséndiz
-//16/Febrero/2023
+//20/Febrero/2023
 
 var comida="Chilaquiles";
 let cena="Tacos";
@@ -10,7 +10,7 @@ console.info("Valor de la comida: " + comida);
 console.warn("El precio no se puede modificar!!");
 console.error("Te dije que el precio no se puede modificar");
 
-console.assert(1==1);//Sirve como parametro una expresi+on lógica, si cumple el programa continua, si no se dejará de ejecutar.
+console.assert(1==1);//Sirve como parametro una expresión lógica, si cumple el programa continua, si no se dejará de ejecutar.
 console.assert(1===0); //"==="" compara valor y tipo de variable.
 console.assert(1=="1");
 console.assert(1== true); //Tienen el mismo valor.
@@ -59,11 +59,9 @@ console.log(arreglo);
 for (let elemento in arreglo){
 	console.log(arreglo);
 }
-
 for (let valor in arreglo){
 	console.log(valor);
 }
-
 	//OBJETOS
 let objeto = {
 	atributo1: "Valor 1",
@@ -73,15 +71,30 @@ console.log(objeto.atributo1);
 console.log(objeto.atributo2);
 console.log(objeto);
 
-
 for (let elemento in objeto){
 	console.log(elemento);
 }
-
 const arregloObjetos=[{atributo1: "Valor 0", atributo2: 1},{}, objeto];
 console.log(arregloObjetos);
-
-
-
-//git add -A
-//git commit -m
+//BOTONES
+document.getElementById("verdes").onclick = () => {
+	let respu = document.getElementById("respuesta");
+	respuesta.innerHTML = "Tienes problemas mentales";
+	//conosle.log("Funciona el botón de chilaquiles verdes"); //El mensaje sale en la terminal.
+	respuesta.classList = []; //Se aiguala a un arreglo vacío para que  no prefiera al danger.
+	respuesta.classList.add ("notification");
+	respuesta.classList.add ("is-danger");
+}
+document.getElementById("rojos").onclick = () => {
+	let respu = document.getElementById("respuesta");
+	respuesta.innerHTML = "Eres una persona feliz";
+	respuesta.classList.add ("notification");
+	respuesta.classList.add ("is-info");
+}
+document.getElementById("ingredientes").onkeyup =() =>{ //Cuando se deje de presionar. Las actividades suceden justo en el momento. 
+	//console.log("Tecla presionada");
+	let input = document.getElementById('ingredientes');
+	let colores = ["blue", "red","green","yellow","cyan","grey","black","purple","white"];
+	let color = Math.floor(Math.random() * colores.lenght);
+	input.style.color = colores[color];
+}
--- Lab4.js
+++ Lab4.js
@@ -1,21 +1,43 @@
+//Ximena Cantera Reséndiz
+//EJERCICIO 1
 let num = prompt("Dame un número",0);
-
-const arreglo = [1];
-
-function cuadrado(a, ){
-        let a = parseInt(num);
-        let result = a * a;
-        alert(result);
-        arreglo.push(result);
-        for (let elemento in arreglo){
-            console.log(arreglo);
-        }
-    }
-//    function cubo(a, b){
-//        var a = parseInt(numerando);
-//        var b = parseInt(numerado);
-//        var result = a - b;
-//        alert(result);
-//        alert('El resultado de ' + a + ' menos ' + b + ' es igual a ' + result);
-//    }
-
+//CUADRADO
+const cuadradoo = [];
+for (let i=1; i <= num; i++){
+    let cuad = i*i;
+    cuadradoo.push(cuad);
+}
+document.write("CUADRADOS<br>");
+for(var i=1; i<=cuadradoo.length; i++){
+    document.write(i + " ^2 " + "= " + cuadradoo[i-1] + "<br>");
+}
+document.write("<br><br>");
+document.write("CUBOS<br>");
+//CUBO
+const cuboo = [];
+for (let i=1; i <= num; i++){
+    let cubo = i*i*i;
+    cuboo.push(cubo);
+}
+for(var i=1; i<=cuboo.length; i++){
+    document.write(i + " ^3 " + "= " + cuboo[i-1] + "<br>");
+}
+//EJERCICIO 2
+let num1 = Math.floor(Math.random()*100+1);
+let num2 = Math.floor(Math.random()*100+1);
+let a = parseInt(num1);
+let b = parseInt(num2);
+//console.time("loop");
+var inicio = Date.now();
+let pregunta = prompt("Adivina: "+num1+"+ "+num2+"=","?");
+//console.timeEnd("loop");
+let fin = Date.now();
+let total = ((fin - inicio)/1000);
+let res = a+b;
+if (pregunta == res) {
+    let correcto = alert("CORRECTO!!     Te tardaste: " + total + " Segundos");
+} else {
+    let incorrecto = alert("INCORRECTO!!    Te tardaste: " + total + " Segundos");
+}
+//EJERCICIO 3
+const arr = [];

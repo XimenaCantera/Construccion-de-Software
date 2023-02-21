9cb03b6e4a3b42427ca7d2dba33f28545425dd6b
XimenaCantera <95453390+XimenaCantera@users.noreply.github.com>
2023-02-20 23:52:53 -0600

    Update Lab4.js

--- Lab4.js
+++ Lab4.js
@@ -1,60 +1,41 @@
+//Ximena Cantera Reséndiz
 //EJERCICIO 1
 let num = prompt("Dame un número",0);
-const aaaaa = [];
-const bbb = [];
-let a = parseInt(num);
-function cuadrado(a){
-    for (let i = 0; i<=a; i++) {
-        let result = a * a;
-        aaaaa.push(result);
-        alert(result);
-    }
-    for (let elemento in aaaaa)
-        console.log(aaaaa);
-function cubo(a){
-    for (let i = 0; i<=a; i++) {
-        let res = a * a;
-        let total = res+a;
-        bbb.push(total);
-        alert(total);
-    }
-    for (let elemento in bbb)
-        console.log(bbb);
+//CUADRADO
+const cuadradoo = [];
+for (let i=1; i <= num; i++){
+    let cuad = i*i;
+    cuadradoo.push(cuad);
+}
+document.write("CUADRADOS<br>");
+for(var i=1; i<=cuadradoo.length; i++){
+    document.write(i + " ^2 " + "= " + cuadradoo[i-1] + "<br>");
+}
+document.write("<br><br>");
+document.write("CUBOS<br>");
+//CUBO
+const cuboo = [];
+for (let i=1; i <= num; i++){
+    let cubo = i*i*i;
+    cuboo.push(cubo);
+}
+for(var i=1; i<=cuboo.length; i++){
+    document.write(i + " ^3 " + "= " + cuboo[i-1] + "<br>");
 }
 //EJERCICIO 2
-function display_c(start){ 
-window.start = parseFloat(start); 
-var end = 0  
-var refresh=1000; 
-if(window.start >= end ){ 
-    mytime=setTimeout('display_ct()',refresh) 
-} else { 
-    alert("Time Over "); 
-    } 
-} 
-function tiempo() { 
-    var dias=Math.floor(window.start / 86400); 
-    var horas = Math.floor((window.start - (dias * 86400 ))/3600); 
-    var minutos = Math.floor((window.start - (dias * 86400 ) - (horas *3600 ))/60);
-    var seg = Math.floor((window.start - (dias * 86400 ) - (horas *3600 ) - (minutos*60)));
-    var z = window.start + "(" + dias + " Días " + horas + " Horas " + minutos + " Minutos y " + seg + " Segundos " + ")"; 
-    window.start= window.start- 1; 
-    tt=display_c(window.start); 
-} 
-let num1 = Math.random();
-let num2 = Math.random();
+let num1 = Math.floor(Math.random()*100+1);
+let num2 = Math.floor(Math.random()*100+1);
 let a = parseInt(num1);
 let b = parseInt(num2);
-let num = prompt("Adivina: "+num1+"+ "+num2+"=","?");
+//console.time("loop");
+var inicio = Date.now();
+let pregunta = prompt("Adivina: "+num1+"+ "+num2+"=","?");
+//console.timeEnd("loop");
+let fin = Date.now();
+let total = ((fin - inicio)/1000);
 let res = a+b;
-function parar() { 
-    clearTimeout(mytime); 
-}
-if (num = res) {
-    let correcto = prompt("CORRECTO!!");
+if (pregunta == res) {
+    let correcto = alert("CORRECTO!!     Te tardaste: " + total + " Segundos");
 } else {
-    let correcto = prompt("INCORRECTO!!");
+    let incorrecto = alert("INCORRECTO!!    Te tardaste: " + total + " Segundos");
 }
-//EJERCICIO 3
-const arr =[];
-let num1 = Math.random();

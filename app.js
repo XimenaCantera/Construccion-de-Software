8e1d5e12fd27ccfb8412e22caf04978333339224
Ximena Cantera <A01277310@tec.mx>
2023-02-23 11:09:41 -0600

    Lab 6

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ console.error("Así se ve un error");
 //fs es el módulo para trabajar con el sistema de archivos
 const filesystem = require('fs');
 filesystem.writeFileSync('Hola .txt', 'Hola desde node');
+
+//ARREGLOS Y TIEMPO DE EJECUCIÓN
+
+//HTTP es el módulo que permite crear un servidor que pueda atender peticiones http
+const http = require('http');  //Crear servidor.(objeto de ejecucio´, objeto de la respueste)
+
+const server = http.createServer( (request,response) => {
+	console.log(request.url);
+	response.setHeader('Content-Type', 'text/html');
+	
+}
+);
+server.listen(3000);

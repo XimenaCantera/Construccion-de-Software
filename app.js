e38b08b7ef4609d16d085ae541642c9bc5847128
Ximena Cantera <A01277310@tec.mx>
2023-02-27 11:34:33 -0600

    Complemento app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Reaccionar a distinas rutas, errores, cambiar datos de hexadecimal a un String, 
+//con la respuesta del cliente se pueden mostrar distintas cosas.
 console.log("Hola mundo desde Node");
 console.info("Este script se estará ejecutando desde una computadora y no desde el navegador :)");
 console.warn("Document, alert, confirm y prompt no existen en el entorno de ejecución de Node");
@@ -26,9 +28,11 @@ const server = http.createServer( (request,response) => {
     radio += '<div><input type="radio" id = "verdes" name="tipo_chilaquiles" value="verdes">';
 	//For contene el id del elemento que se quiere tener referencia, sirve para oder seleccioner desde el nombre. 
     radio += '<label for ="verdes">Salsa verde</label></div>'; 
-
+//"Gracias por tu orden :)"
 	response.write('</form>');
 	response.end();
+	//Se pueden proteger las imágenes para que no se puedan usar en otros códigos/páginas
+	//Llega al servidor en hexadecimal y se transforma a un dato tipo String.
 }
 );
 server.listen(3000);

f4f21259e78cd32414237bce8f4830056097ab81
Ximena Cantera <A01277310@tec.mx>
2023-02-26 18:49:28 -0600

    Laboratorio 6

--- L6.js
+++ L6.js
@@ -1,9 +1,26 @@
-let skz = document.getElementById("oddinary");
-let bts = document.getElementById("tmbm");
-let cg = document.getElementById("conan");
-let fresas = document.getElementById("fresas");
-let pay = document.getElementById("payLimon");
-let helado = document.getElementById("helado");
-let blusa = document.getElementById("blusa");
-let pantalon = document.getElementById("pantalon");
-let chamarra = document.getElementById("chamarra");
+
+function cantidadProductos(valor, value) {
+    let total = 0;
+    let pagar = 0;  
+    valor = parseInt(valor);
+    total = document.getElementById('total').innerHTML;
+    pagar = document.getElementById('pagar').innerHTML;
+    
+    //Valido si hay un valor previo, si no se pone un cero "0".
+    total = (total == null || total == undefined || total == "") ? 0 : total;
+    pagar = (pagar == null || pagar == undefined || pagar == "") ? 0 : pagar;
+    total = (parseInt(total) + parseInt(valor));
+    pagar = parseInt(pagar) + (parseInt(value) * parseInt(valor));
+    document.getElementById('total').innerHTML = total;
+    document.getElementById('pagar').innerHTML = pagar;
+}
+
+function normal(id) {
+  let something = document.getElementById('productos-'+id);
+  productos.style.opacity = "0.5";
+}
+
+function hover(id) {
+  let something = document.getElementById('productos-'+id);
+  productos.style.color = rgba(58, 21, 151, 1);
+}

74a46327e1bd13acb0f08c34067864b6e3eccb57
Ximena Cantera <A01277310@tec.mx>
2023-03-01 21:15:16 -0600

    app.js

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-//Reaccionar a distinas rutas, errores, cambiar datos de hexadecimal a un String, 
-//con la respuesta del cliente se pueden mostrar distintas cosas.
-console.log("Hola mundo desde Node");
-console.info("Este script se estará ejecutando desde una computadora y no desde el navegador :)");
-console.warn("Document, alert, confirm y prompt no existen en el entorno de ejecución de Node");
-console.error("Así se ve un error");
+//Hockey
+const express = require('express');
+const bodyParser = require('body-parser');
 
-//fs es el módulo para trabajar con el sistema de archivos
-const filesystem = require('fs');
-filesystem.writeFileSync('Hola .txt', 'Hola desde node');
+const app = express();
 
-//ARREGLOS Y TIEMPO DE EJECUCIÓN
+app.use(bodyParser.urlencoded({extended: false}));
 
-//HTTP es el módulo que permite crear un servidor que pueda atender peticiones http
-const http = require('http');  //Crear servidor.(objeto de ejecucio´, objeto de la respueste)
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
 
-const server = http.createServer( (request,response) => {
-	console.log(request.url);
-	response.setHeader('Content-Type', 'text/html');
-	response.write('<form>');
-	response.write('<h1>Chilaquiles</h1>');
-	response.write('<form>');
-	response.write('<input type= "radio">');
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    next();
+});
+
+app.use('/hola', (request, response, next) => {
+    response.send('Hola desde la ruta /hola');
+});
+
+const hockeyRutas = require('./routes/hockey.routes');
+
+app.use('/hockey', hockeyRutas);
+
+app.use((request, response, next) => {
+    console.log("Tercer middleware");
+
+    response.status(404);
     
-    let radios = '<field><legend>Selecciona el tipo de chilaquiles:</legend';
-    radio += '<div><input type="radio" id = "rojos" name="tipo_chilaquiles" value="rojos" checked>';
-    radio += '<label for ="rojos">Salsa roja</label></div>';
-    radio += '<div><input type="radio" id = "verdes" name="tipo_chilaquiles" value="verdes">';
-	//For contene el id del elemento que se quiere tener referencia, sirve para oder seleccioner desde el nombre. 
-    radio += '<label for ="verdes">Salsa verde</label></div>'; 
-//"Gracias por tu orden :)"
-	response.write('</form>');
-	response.end();
-	//Se pueden proteger las imágenes para que no se puedan usar en otros códigos/páginas
-	//Llega al servidor en hexadecimal y se transforma a un dato tipo String.
-}
-);
-server.listen(3000);
+    //Envía la respuesta al cliente
+    response.send('Lo sentimos, esta ruta no existe');
+});
+
+app.listen(3000);

865c5c20df5c932987a7045d534c88e27545c2c3
Ximena Cantera <A01277310@tec.mx>
2023-03-02 09:54:04 -0600

    Cambios Lab 12

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+//02/03/2023
 //Hockey
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = reqi¿uire('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
 

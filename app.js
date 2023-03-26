c0c2b0ce7723813435704a194e8f068aa0bd8d81
Ximena Cantera <A01277310@tec.mx>
2023-03-25 21:27:11 -0600

    Rutas hechas y 404

--- app.js
+++ app.js
@@ -1,37 +1,83 @@
-//Hockey
-const express = require('express');
-const bodyParser = require('body-parser');
+ //http es el módulo que permite crear un servidor que pueda atender peticiones http
+ const http = require('http');
+ 
+ const server = http.createServer( (request, response) => {    
+ 
+     console.log(request.url);
+ 
+     if(request.url === "/") {
+         response.setHeader('Content-Type', 'text/html');
+         response.write('<!DOCTYPE html>');
+         response.write('<html>');
+         response.write('<head><meta charset="utf-8"></head><body>');
+         response.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">');
 
-const app = express();
+         response.write("<h1>Chilaquiles</h1>");
+         response.write('<a href="/ordenar">Ordena tus chilaquiles aquí</a>');
+         response.end();
+     } else if (request.url === "/ordenar" && request.method === "GET") {
+         response.setHeader('Content-Type', 'text/html');
+         response.write('<!DOCTYPE html>');
+         response.write('<html>');
+         response.write('<head><meta charset="utf-8"></head><body>');
+         response.write("<h1>Chilaquiles</h1>");
+         response.write('<form action="/ordenar" method="POST">');
+ 
+         let radios = '<fieldset><legend>¿Con qué salsa quieres tus chilaquiles?</legend>';
+         radios += '<div><input type="radio" id="rojos" name="tipo_chilaquiles" value="rojos" checked>'
+         radios += '<label for="rojos">Salsa roja</label></div>';
+         radios += '<div><input type="radio" id="verdes" name="tipo_chilaquiles" value="verdes">';
+         radios += '<label for="verdes">Salsa verde</label></div></fieldset><br>';
+ 
+         response.write(radios);
+ 
+         response.write('<label for="extras">¿Con qué deseas tus chilaquiles?</label>');
+         response.write('<input type="text" id="extras" name="extras"><br><br>');
+ 
+         response.write('<input type="submit" value="Ordenar">');
+ 
+         response.write("</form>");
+         response.write('</body></html>');
+         response.end();
+     } else if (request.url === "/ordenar" && request.method === "POST") {
 
-app.use(bodyParser.urlencoded({extended: false}));
+        const datos = [];
 
-//Middleware
-app.use((request, response, next) => {
-    console.log('Middleware!');
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
-});
+        request.on('data', (dato) => {
+            console.log(dato);
+            datos.push(dato);
+        });
 
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    next();
-});
+        return request.on('end', () => {
+            const datos_completos = Buffer.concat(datos).toString();
+            console.log(datos_completos);
+            const tipo_chilaquiles = datos_completos.split('&')[0].split('=')[1];
+            console.log(tipo_chilaquiles);
+            if(tipo_chilaquiles === "rojos") {
+                response.setHeader('Content-Type', 'text/html');
+                response.write('<!DOCTYPE html>');
+                response.write("Gracias por tu orden");
+                response.write('<img alt="chilaquiles rojos" src="https://sazondemama.com/wp-content/uploads/2022/09/Como-hacer-la-receta-de-chilaquiles-rojos-y-cuantas-calorias-tiene-768x432.jpg">');
+                return response.end();
+            } else {
+                response.setHeader('Content-Type', 'text/html');
+                response.write('<!DOCTYPE html>');
+                response.write("Gracias por tu orden");
+                response.write('<img alt="chilaquiles verdes" src="https://i.pinimg.com/736x/9a/c3/2b/9ac32b9b26902dc6708d835d6b8d0954.jpg">');
+                return response.end();
+            }
+            
+        });
 
-app.use('/hola', (request, response, next) => {
-    response.send('Hola desde la ruta /hola');
-});
-
-const hockeyRutas = require('./routes/hockey.routes');
-
-app.use('/hockey', hockeyRutas);
-
-app.use((request, response, next) => {
-    console.log("Tercer middleware");
-
-    response.status(404);
-    
-    //Envía la respuesta al cliente
-    response.send('Lo sentimos, esta ruta no existe');
-});
-
-app.listen(3000);
+        
+     } else {
+         response.statusCode = 404;
+         response.write("Lo sentimos, de esos chilaquiles no tenemos");
+         response.end();
+     }
+ 
+     
+     
+ });
+ 
+ server.listen(3000);
--- app10.js
+++ app10.js
@@ -1,23 +1,148 @@
-const filesystem = require('fs');
-filesystem.writeFileSync('hola.txt', 'Hola desde node');
-
-
-const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
-
-for (let item of arreglo) {
-    setTimeout(() => {
-        console.log(item);
-    }, item);
-} 
-
-
 const http = require('http');
+ 
+ const server = http.createServer( (request, response) => {    
+ 
+     console.log(request.url);
+ 
+     if(request.url === "/") {
+         response.setHeader('Content-Type', 'text/html');
+         response.write('<!DOCTYPE html>');
+         response.write('<html>');
+         response.write('<head><meta charset="utf-8"></head><body>');
+        
+         response.write('<table class="default">');
+		response.write('<caption>Principales hobbys y mi Top 3</caption>');
+		response.write('<tr>');
+	    response.write('<th>Escuchar música</th>');
+	    response.write('<th>Ver películas</th>');
+	    response.write('<th>Leer</th>');
+	    response.write('<th>Jugar</th>');
+	    response.write('</tr>');
+	   	response.write('<tr>');
+	    response.write('<td>Stray Kids</td>');
+	    response.write('<td>Terror y suspenso</td>');
+	    response.write('<td>The Loop</td>');
+	    response.write('<td> Clash Of Clans </td>');
+	    response.write('</tr>');
+	    response.write('<tr>');
+	    response.write('<td>Harry Styles</td>');
+	    response.write('<td>Ciencia ficción</td>');
+	    response.write('<td>El jardín de las mariposas</td>');
+	    response.write('<td>Valorant</td>');
+	  	response.write('</tr>');
+	  	response.write('<tr>');
+	    response.write('<td>The Weekend</td>');
+	    response.write('<td>Aventuras</td>');
+	    response.write('<td>La biblioteca de la media noche</td>');
+	    response.write('<td>Minecraft</td>');
+	    response.write('</tr>');
+	    response.write('</table>');
+		response.write('<p style="color: rgba(168, 0, 0, 0.84)">¿En qué coincidimos en gustos?</p>');
+	    response.write("</form>");
 
-const server = http.createServer( (request, response) => {    
-console.log(request.url);
-response.setHeader('Content-Type', 'text/html');
-    response.write("");
-    response.end();
-});
+	    response.write('<a class="button is-warning is-light is-outlined is-normal" href="/enviar">Sugerir</a>');
 
-server.listen(3000);
+	    response.end();
+	    } else if (request.url === "/enviar" && request.method === "GET") {
+         response.setHeader('Content-Type', 'text/html');
+         response.write('<!DOCTYPE html>');
+         response.write('<html>');
+         response.write('<head><meta charset="utf-8"></head><body>');
+        	
+        let radios = '<fieldset><legend>¿Cuál te gs¿usta más?</legend>';
+
+        response.write('<form action="/enviar" method="POST">');
+
+         response.write('<div class="columns">');
+	  	 response.write('<div class="column">');
+	  	 response.write('<h5 class="title is-5">Música</h5>');
+		 response.write('<form>');
+		 response.write('<input type="checkbox" name="Gusto" value="JONGHYUN">');
+		 response.write('<label for="L3">Just Chill - JONGHYUN</label><br>');
+		
+		response.write('<h5 class="title is-5">Películas</h5>');
+		response.write('<form>');
+		response.write('<input type="checkbox" name="Gusto" value="Duna">');
+		response.write('<label for="L4">Duna</label><br><br>');
+			  
+
+	    response.write('<div class="column">');
+		response.write('<article class="message">');
+		response.write('<div class="message-header">');
+		response.write('<p>Just Chill - JONGHYUN</p>');
+		response.write('</div>');
+		response.write('<div class="message-body">');
+		response.write('Oh, the sound of the rain has come to me<br>Pit pat, it knocks on my window<br>But I don’t Wanna open<br>I wanna be alone<br>Let me be still just for today<br>I’m gonna turn on the AC and get under the covers<br>I wanna be wasteful...<br>');
+		response.write('</div>');
+		response.write('</article>');
+
+		response.write('<article class="message">');
+		response.write('<div class="message-header">');
+		response.write('<p>DUNA</p>');
+		response.write('</div>');
+		response.write('<div class="message-body">');
+		response.write('“Duna” cuenta la historia de Paul Atreides, un joven brillante, talentoso y con un destino que va más allá de su comprensión, quien debe viajar al planeta más peligroso del universo para garantizar el futuro de su familia y de su gente.');
+		response.write('</div>');
+		response.write('</article>');
+
+	  	response.write('</div>');
+  		response.write('</div>');
+        
+ 
+        response.write('<br><input type="submit" value="Enviar">');
+ 
+        response.write("</form>");
+        response.write('</body></html>');
+        response.end();
+
+     } else if (request.url === "/enviar" && request.method === "POST") {
+
+        const datos = [];
+
+        request.on('data', (dato) => {
+            console.log(dato);
+            datos.push(dato);
+        });
+
+        return request.on('end', () => {
+            const datos_completos = Buffer.concat(datos).toString();
+            console.log(datos_completos);
+            const Gusto = datos_completos.split('&')[0].split('=')[1];
+            console.log(Gusto);
+            if(Gusto === "JONGHYUN") {
+                response.setHeader('Content-Type', 'text/html');
+                response.write('<!DOCTYPE html>');
+                response.write('<html>');
+         		response.write('<head><meta charset="utf-8"></head><body>');
+                response.write("Qué buenos gustos tienes!!");
+                response.write('<br><img alt="JONGHYUN" src="https://i.pinimg.com/564x/1a/eb/d9/1aebd96f7e29bf7fea42599af754908e.jpg">');
+                return response.end();
+            } else {
+                response.setHeader('Content-Type', 'text/html');
+                response.write('<!DOCTYPE html>');
+                response.write('<html>');
+         		response.write('<head><meta charset="utf-8"></head><body>');
+                response.write("¿Qué te pareció la película?");
+                response.write('<br><img alt="duna" src="https://i.pinimg.com/564x/98/27/ae/9827ae0ed42031dead8d75e3d46ef933.jpg">');
+                return response.end();
+            }
+            
+        });
+
+        
+     } else {
+         response.statusCode = 404;
+         response.setHeader('Content-Type', 'text/html');
+         response.write('<!DOCTYPE html>');
+         response.write('<html>');
+   		 response.write('<head><meta charset="utf-8"></head><body>');
+         response.write("A dónde intentas ir???");
+         response.write('<br><img src="https://i.pinimg.com/564x/e3/5c/83/e35c83c38269d60ec602f06c2d11eb2e.jpg">');
+         response.end();
+     }
+ 
+     
+     
+ });
+
+  server.listen(4000);

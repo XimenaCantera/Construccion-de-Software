7b940839aa2f98b0d33534c8e56e9b0cea959870
Ximena Cantera <A01277310@tec.mx>
2023-03-27 20:41:58 -0600

    L12 HTML dinámico e instalación EJS

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
+//SOLISTAS, GRUPOS y GUSTOS
+
 const express = require('express');
- const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
  
- const app = express();
+const app = express();
  
- app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
  
- app.use((request, response, next) => {
-     console.log('Middleware!');
-     next(); //Le permite a la petición avanzar hacia el siguiente middleware
+ //Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); 
  });
+ 
 
-//      MÓDULO 1-. Grupos      /grupo/nuevo
-const grupoRutas = require('./routes/grupos.routes');
-
-app.use('/grupo', grupoRutas);
-
-
-//      MÓDULO 2-. Solistas      /solista/nuevo
+//           solistas/nuevo 
 const solistaRutas = require('./routes/solistas.routes');
-
 app.use('/solista', solistaRutas);
 
+//            grupo/nuevo
+const grupoRutas = require('./routes/grupos.routes');
+app.use('/grupo', grupoRutas);
 
+//      gustos.html   ->     /gustos/comparar
+const gustosRutas = require('./routes/gustos.routes');
+app.use('/gustos', gustosRutas);
 
-//ERROR 404
- app.use((request, response, next) => {
+       
+app.use((request, response, next) => {
     response.status(404);
     let html = `
-        <body class="selection:bg-pink-300  bg-green-500"> 
-        <script src="https://cdn.tailwindcss.com"></script>
-            <p class="font-mono text-2xl underline decoration-red-500"> ¿A dónde intentas ir? No tienes que iniciar aquí -_-</p>
-            <br>
-            <img class="scale-50 float-center rounded-md" src="https://i.pinimg.com/564x/e3/5c/83/e35c83c38269d60ec602f06c2d11eb2e.jpg">
-        </body>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <div class="bg-fixed" style="background-image: url(https://sourcecode.mx/wp-content/uploads/2020/10/error-404-768x427.png)"></div>
+    <div class="grid justify-items-center mt-16">
+        <p class="font-mono text-2xl">No tienes que iniciar aquí</p>
+    </div>
+
     `;
+
     response.send(html);
- });
- 
- app.listen(8000)
+});
+
+app.listen(5000);
